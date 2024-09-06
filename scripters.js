911466260791996b1d36f685680fc5987b511a95
VectorBatesta <vitorbatista@alunos.utfpr.edu.br>
2024-09-06 03:33:02 -0300

    faster button created

--- scripters.js
+++ scripters.js
@@ -4,7 +4,6 @@ import { gameVariables } from "./listeners.js"
 
 
 export function incrementar() {
-    console.log(gameVariables)
     gameVariables.clicksFodido++;
     document.getElementsByTagName("button")[0].textContent = gameVariables.clicksFodido + " vez fodido";
 }
@@ -26,3 +25,31 @@ document.getElementsByTagName("body")[0].appendChild(newElement);
 
 
 //const newElement = document.createElement("");
+
+
+
+function intervaloIncrementa(){
+    incrementar()
+    clearInterval(intervaloSetado)
+    intervaloSetado = setInterval(intervaloIncrementa, time)
+}
+
+var time = 1000
+var intervaloSetado = setInterval(intervaloIncrementa, time);
+
+
+
+
+
+
+
+
+function faster(){
+    time--
+    console.log(time)
+}
+
+const fasterButton = document.createElement("button");
+fasterButton.textContent = "faster!!";
+fasterButton.onclick = faster;
+document.getElementsByTagName("body")[0].appendChild(fasterButton);
